0c686d0f2f5f2ffa90f9fcdfef544cfd2aa74fba
kaarthiksundar <kaarthik01sundar@gmail.com>
2026-01-26 10:31:27 -0700

    result organization

--- readResults/queries.py
+++ readResults/queries.py
@@ -21,7 +21,7 @@ class databaseToCSV():
         self.cursor = self.connection.cursor()
         self.results_path = results_path
 
-    def _closeConnection(self):                
+    def _closeConnection(self):
         self.cursor.close()
         self.connection.close()
 
@@ -32,8 +32,8 @@ class databaseToCSV():
             WHERE instanceName = ? AND numVehicles = ? AND objective = ? AND pNorm = ? AND fairnessCoefficient = ?
         """, (instance_name, str(numVehicles), objective, str(pNorm), str(fc)))
         result = self.cursor.fetchone()
-        return result[0] if result else None 
-      
+        return result[0] if result else None
+
     def _getSumofTours(self, instance_name, objective, numVehicles, pNorm = 1, fc = 0.0):
         self.cursor.execute(f"""
             SELECT SumOfTours
@@ -41,8 +41,8 @@ class databaseToCSV():
             WHERE instanceName = ? AND numVehicles = ? AND objective = ? AND pNorm = ? AND fairnessCoefficient = ?
         """, (instance_name, str(numVehicles), objective, str(pNorm), str(fc)))
         result = self.cursor.fetchone()
-        return round(float(result[0]),1) if result else None 
-    
+        return round(float(result[0]),1) if result else None
+
     def _getLengthofTours(self, instance_name, objective, numVehicles, pNorm = 1, fc = 0.0):
         self.cursor.execute(f"""
             SELECT LengthOfTours
@@ -52,19 +52,19 @@ class databaseToCSV():
         result = self.cursor.fetchone()
         if result:
             result = ast.literal_eval(result[0])
-        return [round(abs(len),1) for len in result] if result else None 
+        return [round(abs(len),1) for len in result] if result else None
 
     def _getFairnessIndex(self, instance_name, numVehicles, objective, pNorm = 1, fc = 0.0):
-        # returns jainIndex, giniIndex and normIndex corresponding to given instance 
+        # returns jainIndex, giniIndex and normIndex corresponding to given instance
         self.cursor.execute(f"""
                 SELECT GiniIndex, JainIndex, normIndex
                 FROM {'vehi'+str(numVehicles)}
                 WHERE instanceName = ? AND numVehicles = ? AND objective = ? AND pNorm = ? AND fairnessCoefficient = ?
                 """,  (instance_name, str(numVehicles), objective, str(pNorm), str(fc)))
         result = self.cursor.fetchone()
-        fairIndex = {'giniIndex': float(result[0]), 'jainIndex':float(result[1]), 'normIndex':float(result[2])} if result else None 
+        fairIndex = {'giniIndex': float(result[0]), 'jainIndex':float(result[1]), 'normIndex':float(result[2])} if result else None
         return fairIndex
-    
+
     def _getMinParam(self, instance_name, numVehicles, objective):
         self.cursor.execute(f"""
             SELECT MIN(fairnessCoefficient)
@@ -73,7 +73,7 @@ class databaseToCSV():
         """, (instance_name, str(numVehicles), objective))
         result = self.cursor.fetchone()
         return round(float(result[0]),4) if result else None
-    
+
     def _getMaxParam(self, instance_name, numVehicles, objective):
         self.cursor.execute(f"""
             SELECT MAX(fairnessCoefficient)
@@ -82,7 +82,7 @@ class databaseToCSV():
         """, (instance_name, str(numVehicles), objective))
         result = self.cursor.fetchone()
         return round(float(result[0]),4) if result else None
-    
+
     def _getCoefficientOfVariation(self, lengthOfTours):
         mean = np.mean(lengthOfTours)
         std = np.std(lengthOfTours)
@@ -131,12 +131,12 @@ class databaseToCSV():
                                 data.append(round(comp_time,2)) if comp_time<3600 else data.append('-')
 
                         if objective in ['eps-fair', 'delta-fair']:
-                            fairnessCoefficient = [0.1,0.3,0.5,0.7,0.9] 
+                            fairnessCoefficient = [0.1,0.3,0.5,0.7,0.9]
                             for fc in fairnessCoefficient:
                                 comp_time = float(self._getComputationTime(instance_name=instance_name, objective=objective, numVehicles=numVehicle, fc=fc))
                                 # data.append(round(comp_time/min_time, 2)) if comp_time<3600 else data.append('-')
                                 data.append(round(comp_time,2)) if comp_time<3600 else data.append('-')
-                        
+
                     csv_writer.writerow(data)
 
     def export_all_COF_to_csv(self):
@@ -145,10 +145,10 @@ class databaseToCSV():
 
         with open(csv_filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-           
+
             csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'min (cost)', 'minmax ', 'pNorm 2', 'pNorm 3', 'pNorm 5', 'pNorm 10',
                                 'epsFair 0.1', 'epsFair 0.3', 'epsFair 0.5', 'epsFair 0.7', 'epsFair 0.9', 'deltaFair 0.1', 'deltaFair 0.3', 'deltaFair 0.5', 'deltaFair 0.7', 'deltaFair 0.9'])  # Write header
-            
+
             data = []
             for instance_name in ['burma14.tsp', 'bays29.tsp', 'eil51.tsp', 'eil76.tsp']:
                 for numVehicle in [3,4,5]:
@@ -170,13 +170,13 @@ class databaseToCSV():
                                 data.append(round((cost-min_cost)/min_cost, 2))
 
                         if objective in ['eps-fair', 'delta-fair']:
-                            fairnessCoefficient = [0.1,0.3,0.5,0.7,0.9] 
+                            fairnessCoefficient = [0.1,0.3,0.5,0.7,0.9]
                             for fc in fairnessCoefficient:
                                 cost = float(self._getSumofTours(instance_name=instance_name, objective=objective, numVehicles=numVehicle, fc=fc))
                                 data.append(round((cost-min_cost)/min_cost, 2))
-                        
+
                     csv_writer.writerow(data)
-    
+
     def export_COF_plotdata(self):
 
         csv_filename = os.path.join(self.results_path, 'COF_plotdata.csv')
@@ -188,18 +188,18 @@ class databaseToCSV():
             min_cost = self._getSumofTours(instance_name=instance_name, objective='min', numVehicles=numVehicle)
             minmax_cost = self._getSumofTours(instance_name=instance_name, objective='min-max', numVehicles=numVehicle)
             minmax_COF = round((minmax_cost-min_cost)/min_cost, 2)
-            csv_writer.writerows([['instanceName', instance_name], ['numVehicles', numVehicle ], 
+            csv_writer.writerows([['instanceName', instance_name], ['numVehicles', numVehicle ],
                                   ['minmaxCOF', minmax_COF],['fairnessCoefficient', 'epsCOF', 'deltaCOF']])
 
             fairnessCoefficient = [round(x,2) for x in np.arange(0,1,0.05)]
 
             for fc in fairnessCoefficient:
                 data = [fc]
-                for objective in ['eps-fair', 'delta-fair']:   
+                for objective in ['eps-fair', 'delta-fair']:
                     cost = self._getSumofTours(instance_name=instance_name, objective=objective, numVehicles=numVehicle, fc=fc)
                     data.append(round((cost-min_cost)/min_cost, 2))
                 csv_writer.writerow(data)
-        
+
             csv_writer.writerow([1.0, 0.28, 0.0])
 
     def export_ParetoFront_plotdata(self):
@@ -219,7 +219,7 @@ class databaseToCSV():
 
             for fc in fairnessCoefficient:
                 data = [fc]
-                for objective in ['eps-fair', 'delta-fair']:   
+                for objective in ['eps-fair', 'delta-fair']:
                     cost = self._getSumofTours(instance_name=instance_name, objective=objective, numVehicles=numVehicle, fc=fc)
                     data.append(round(cost, 2))
                 csv_writer.writerow(data)
@@ -232,9 +232,9 @@ class databaseToCSV():
 
         with open (csv_filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'minmax time', 'minmax cost', 'minmax eps', 'minmax delta', 
+            csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'minmax time', 'minmax cost', 'minmax eps', 'minmax delta',
                                  'epsFair time', 'epsFair cost', 'epsFair fc', 'epsFair eps', 'deltaFair time', 'deltaFair cost', 'deltaFair fc', 'deltaFair delta' ])
-            
+
             data = []
             for instance_name in ['bays29.tsp', 'eil51.tsp', 'eil76.tsp']:
                 for numVehicle in [3,4,5]:
@@ -268,13 +268,13 @@ class databaseToCSV():
 
         with open (csv_filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'minmax cost', 'minmaxCOF', 'minmax eps', 'minmax delta', 
+            csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'minmax cost', 'minmaxCOF', 'minmax eps', 'minmax delta',
                                  'epsFair cost', 'epsFair COF', 'deltaFair cost', 'deltaFair COF' ])
-            
+
             data = []
             for instance_name in ['burma14.tsp','bays29.tsp', 'eil51.tsp']:
                 for numVehicle in [3,4,5]:
-                    
+
                     min_cost = self._getSumofTours(instance_name=instance_name, objective='min', numVehicles=numVehicle)
 
                     comp_time = self._getComputationTime(instance_name=instance_name, objective='min-max', numVehicles=numVehicle)
@@ -302,9 +302,9 @@ class databaseToCSV():
 
         with open (csv_filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Instance Name', 'Number of Vehicles', '2Norm time', '2Norm cost', '2Norm eps', '2Norm delta', 
+            csv_writer.writerow(['Instance Name', 'Number of Vehicles', '2Norm time', '2Norm cost', '2Norm eps', '2Norm delta',
                                  'epsFair time', 'epsFair cost', 'epsFair fc', 'epsFair eps', 'deltaFair time', 'deltaFair cost', 'deltaFair fc', 'deltaFair delta' ])
-            
+
             data = []
             for instance_name in ['bays29.tsp', 'eil51.tsp', 'eil76.tsp']:
                 for numVehicle in [3,4,5]:
@@ -338,12 +338,12 @@ class databaseToCSV():
         with open (csv_filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(['Instance Name', 'Number of Vehicles', 'objective', 'parameter', 'l1', 'l2', 'l3', 'l4', 'Coeff Variation'])
-            
+
             data = []
             for instance_name in ['burma14.tsp', 'bays29.tsp', 'eil51.tsp']:
                 numVehicle = 4
                 for objective in ['min', 'min-max', 'p-norm', 'eps-fair', 'delta-fair']:
-                    
+
                     if objective in ['min', 'min-max']:
                         data = [instance_name.split(".")[0], numVehicle]
                         data.extend([objective, '-'])
@@ -366,14 +366,14 @@ class databaseToCSV():
                         if objective == 'eps-fair':
                             fairnessCoefficient = [0.1,0.3,0.5,0.7,0.9, self._getMaxParam(instance_name, numVehicle, objective)]
                         if objective == 'delta-fair':
-                            fairnessCoefficient = [self._getMinParam(instance_name, numVehicle, objective) , 0.1,0.3,0.5,0.7,0.9] 
-                        
+                            fairnessCoefficient = [self._getMinParam(instance_name, numVehicle, objective) , 0.1,0.3,0.5,0.7,0.9]
+
                         for fc in fairnessCoefficient:
-                            data = [instance_name.split(".")[0], numVehicle, objective, fc]        
+                            data = [instance_name.split(".")[0], numVehicle, objective, fc]
                             lengthOfTours = self._getLengthofTours(instance_name=instance_name, objective=objective, numVehicles=numVehicle, fc=fc)
                             data.extend(lengthOfTours)
                             data.append(self._getCoefficientOfVariation(lengthOfTours))
-                    
+
                             csv_writer.writerow(data)
 
 
@@ -382,9 +382,9 @@ class databaseToCSV():
 def handle_command_line():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-tableName", "--tableName", choices=['runtime_pNorm', 'runtime_epsFair', 'runtime_deltaFair', 'COF', 'minmaxFair', 'pNormFair', 'ParetoFront', 'COV'],
+    parser.add_argument("-t", "--tableName", choices=['runtime_pNorm', 'runtime_epsFair', 'runtime_deltaFair', 'COF', 'minmaxFair', 'pNormFair', 'ParetoFront', 'COV'],
                         help="give the table name", type=str)
-    
+
     args = parser.parse_args()
 
     return args.tableName
@@ -396,12 +396,12 @@ def main():
     try:
         folder_path = os.path.dirname(os.path.realpath(__file__))
         base_path = os.path.abspath(os.path.join(folder_path, '..'))
-        results_path = os.path.join(base_path, 'results/round2')
+        results_path = os.path.join(base_path, 'results/round-2')
         db_path = os.path.join(results_path, 'results.db')
-        
+
         tableName = handle_command_line()
         dataTransfer = databaseToCSV(db_path, results_path)
-        
+
         if tableName == 'runtime_pNorm':
             dataTransfer.export_computation_time_to_csv(table_objective='p-norm')
         elif tableName == 'runtime_epsFair':
@@ -414,14 +414,14 @@ def main():
             dataTransfer.export_minmaxFair_final()
         elif tableName == 'pNormFair':
             dataTransfer.export_pNormFair_to_csv()
-        elif tableName == 'ParetoFront':    
+        elif tableName == 'ParetoFront':
             dataTransfer.export_ParetoFront_plotdata()
         elif tableName == 'COV':
             dataTransfer.export_coeff_variation()
-        
+
         dataTransfer._closeConnection()
-    
-    
+
+
     except ScriptException as se:
         log.error(se)
 
